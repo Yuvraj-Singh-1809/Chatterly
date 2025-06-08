4c183736b6f4c7c27f83486ba444fc0fb0573a5b
Yuvraj-Singh-1809 <yuvi.18sept@gmail.com>
2025-06-09 02:51:54 +0530

    Changed app.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import {  UserContextProvider } from "./UserContext";
 
 function App() {
-axios.defaults.baseURL="http://localhost:3000";
+axios.defaults.baseURL="https://chatterly-yt36.onrender.com";
 axios.defaults.withCredentials= true;
   return (
     <UserContextProvider>
