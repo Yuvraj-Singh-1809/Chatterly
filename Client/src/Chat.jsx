f0a57fa0cd17534f714954966f6e4d58f1d14b01
Yuvraj-Singh-1809 <yuvi.18sept@gmail.com>
2025-06-09 03:33:29 +0530

    updated chat.jsx

--- Client/src/Chat.jsx
+++ Client/src/Chat.jsx
@@ -22,7 +22,7 @@ export default function Chat(){
     },[]);
 
      function connectToWs(){
-      const ws=  new WebSocket("ws://chatterly-yt36.onrender.com");
+      const ws=  new WebSocket("wss://chatterly-yt36.onrender.com");
       setWs(ws);
       ws.addEventListener("message",handleMessage);
       ws.addEventListener("close",()=>{
